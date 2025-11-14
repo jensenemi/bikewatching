622c1bba925ec49ae85ea947a0a708e9e12a616b
jensenemi <jeemi@ucsd.edu>
2025-11-13 22:55:18 -0800

    3.1

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 // Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 // Check that Mapbox GL JS is loaded
@@ -43,4 +44,15 @@ map.on('load', async () => {
         source: 'cambridge_route',
         paint: bikeLaneStyle
     });
-  });
+    let jsonData;
+    try {
+        const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
+        // Await JSON fetch
+        const jsonData = await d3.json(jsonurl);
+        console.log('Loaded JSON Data:', jsonData); // Log to verify structure
+        let stations = jsonData.data.stations;
+        console.log('Stations Array:', stations);
+    } catch (error) {
+        console.error('Error loading JSON:', error); // Handle errors
+    }
+});
