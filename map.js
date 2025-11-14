98cc63051ca34cdd4e521e96427f623f0f3c4169
jensenemi <jeemi@ucsd.edu>
2025-11-14 15:58:48 -0800

    Update map.js

--- map.js
+++ map.js
@@ -156,8 +156,7 @@ map.on('load', async () => {
         const circles = svg
             .selectAll('circle')
             .data(stations, (d) => d.short_name)
-            .enter()
-            .append('circle')
+            .join('circle')
             .attr('cx', d => getCoords(d).cx)              
             .attr('cy', d => getCoords(d).cy)    
             .attr('r', d => radiusScale(d.totalTraffic))            
@@ -193,14 +192,15 @@ map.on('load', async () => {
           
             // Recompute station traffic based on the filtered trips
             const filteredStations = computeStationTraffic(stations, filteredTrips);
+
             timeFilter === -1 ? radiusScale.range([0, 25]) : radiusScale.range([3, 50]);
             // Update the scatterplot by adjusting the radius of circles
             circles
               .data(filteredStations, (d) => d.short_name)
               .join('circle') // Ensure the data is bound correctly
               .attr('r', (d) => radiusScale(d.totalTraffic)) // Update circle sizes
-              .select('title')
-              .text(d => `${d.totalTraffic} trips (${d.departures} departures, ${d.arrivals} arrivals)`);
+            //   .select('title')
+            //   .text(d => `${d.totalTraffic} trips (${d.departures} departures, ${d.arrivals} arrivals)`);
         }
         updateTimeDisplay();
     } catch (error) {
