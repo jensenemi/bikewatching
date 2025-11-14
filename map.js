5e3009b2a927b43ae9e4c9540c8fc489876ba2d8
jensenemi <jeemi@ucsd.edu>
2025-11-13 22:47:29 -0800

    2.3

--- map.js
+++ map.js
@@ -16,19 +16,31 @@ const map = new mapboxgl.Map({
   maxZoom: 18, // Maximum allowed zoom
 });
 
+const bikeLaneStyle = {
+    'line-color': '#32D400', 
+    'line-width': 3,
+    'line-opacity': 0.6,
+};
+
 map.on('load', async () => {
     map.addSource('boston_route', {
         type: 'geojson',
         data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
     });
     map.addLayer({
-        id: 'bike-lanes',
+        id: 'boston-bike-lanes',
         type: 'line',
         source: 'boston_route',
-        paint: {
-          'line-color': '#32D400',
-          'line-width': 3,
-          'line-opacity': 0.6,
-        },
-      });
+        paint: bikeLaneStyle
+    });
+    map.addSource('cambridge_route', {
+        type: 'geojson',
+        data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+    });
+    map.addLayer({
+        id: 'cambridge-bike-lanes',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: bikeLaneStyle
+    });
   });
