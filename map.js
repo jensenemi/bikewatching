4bd6e39474579a36630110e8885708dcf22ca57c
jensenemi <jeemi@ucsd.edu>
2025-11-14 19:31:02 -0800

    Update map.js

--- map.js
+++ map.js
@@ -122,8 +122,7 @@ map.on('load', async () => {
         const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
         // Await JSON fetch
         const jsonData = await d3.json(jsonurl);
-        console.log('Loaded JSON Data:', jsonData); // Log to verify structure
-        let stations = jsonData.data.stations;
+
         //within the map.on('load')
         let trips = await d3.csv(
             'https://dsc106.com/labs/lab07/data/bluebikes-traffic-2024-03.csv',
@@ -133,7 +132,7 @@ map.on('load', async () => {
                 return trip;
             },
         );
-        stations = computeStationTraffic(jsonData.data.stations, trips);
+        let stations = computeStationTraffic(jsonData.data.stations, trips);
         const departures = d3.rollup(
             trips,
             (v) => v.length,
